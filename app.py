8d6212d5189789d504db10ad685f7a37b3d924df
Danss0 <rizkydante0@gmail.com>
2023-12-12 20:23:46 +0700

    menambahkan route about

--- app.py
+++ app.py
@@ -177,6 +177,10 @@ def syaratketentuan():
 def carapemesanan():
     return render_template('cara_pemesanan.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
